44ca599f8135318a0a0f62c2eb157e05a8f85d65
a298034a <a298034aa298034a@gmail.com>
2024-08-11 15:52:53 +0800

    首頁完成

---
+++ js/footer.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var footerHTML = `
+    <footer>
+        <p>Copyright © 2024 Chuckle Inc. 保留一切權利。</p>
+    </footer>
+    `;
+    
+    document.body.insertAdjacentHTML('afterend', footerHTML);
+});
