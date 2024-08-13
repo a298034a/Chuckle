eda3a034df85ef2f4fd831ad4cfda24034314e54
a298034a <a298034aa298034a@gmail.com>
2024-08-13 23:59:30 +0800

    會員區塊製作中

--- js/toggle-btn.js
+++ js/toggle-btn.js
@@ -6,9 +6,9 @@ toggle.addEventListener('click', function() {
 	toggleNumber = !toggleNumber;
 	if (toggleNumber) {
 		toggleContainer.style.clipPath = 'inset(0 0 0 50%)';
-		toggleContainer.style.backgroundColor = '#D74046';
 	} else {
 		toggleContainer.style.clipPath = 'inset(0 50% 0 0)';
-		toggleContainer.style.backgroundColor = 'dodgerblue';
 	}
+
+	toggleContainer.style.backgroundColor = 'var(--hover-yellow)';
 });
