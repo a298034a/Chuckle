be7661454523d7a4b238f18587360c8bfadefc58
a298034a <a298034aa298034a@gmail.com>
2024-08-18 23:26:16 +0800

    招牌移動腳本完成 調整圖層

--- js/ShopSignMoving.js
+++ js/ShopSignMoving.js
@@ -1,24 +1,36 @@
 document.addEventListener('scroll', function () {
-    var shopSign = document.querySelector('.shop-sign-left');
-    var shopSignTop = shopSign.getBoundingClientRect().top;
+    updatePositionByDistance();
+});
 
-    if (shopSignTop <= 0) {
-        shopSign.style.position = 'fixed';
-        shopSign.style.top = '0';
+window.addEventListener('resize', () => {
+    updatePositionByDistance();
+});
+
+function updatePositionByDistance() {
+    var shopSignLeft = document.querySelector('.shop-sign-left');
+    var shopSignRight = document.querySelector('.shop-sign-right');
+    var shopSignTop = shopSignLeft.getBoundingClientRect().top;
+
+    if (shopSignTop <= 0) {        
+        shopSignLeft.style.position = 'fixed';
+        shopSignLeft.style.top = '0';
+
+        shopSignRight.style.position = 'fixed';
+        shopSignRight.style.top = '0';
     }
 
     var banner = document.querySelector('.banner');
 
     var bannerRect = banner.getBoundingClientRect();
-    var shopSignRect = shopSign.getBoundingClientRect();
+    var shopSignRect = shopSignLeft.getBoundingClientRect();
 
     var verticalDistance = shopSignRect.top - bannerRect.bottom;
-    
-    if (verticalDistance < -15) {
-        shopSign.style.position = 'absolute';
-        shopSign.style.top = `${400*(window.innerWidth/1980)}px`;//'355px';
-        console.log(shopSign.style.top+" "+window.innerWidth);
-    }
-});
 
-//todo:resize
+    if (verticalDistance < -15) {
+        shopSignLeft.style.position = 'absolute';
+        shopSignLeft.style.top = `${400 * (window.innerWidth / 1980)}px`;
+
+        shopSignRight.style.position = 'absolute';
+        shopSignRight.style.top = `${400 * (window.innerWidth / 1980)}px`;
+    }
+}
