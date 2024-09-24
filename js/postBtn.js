86299b259a5884bfda36d0f82b9c30df98121f33
xpluso <xxxplusooo913@gmail.com>
2024-09-24 16:23:03 +0800

    文章內容-喜歡收藏hover效果

--- js/postBtn.js
+++ js/postBtn.js
@@ -1,24 +1,54 @@
 // 點喜歡
 let love = document.getElementById('love');
+let isClicked = false; // 用來追蹤是否點擊過
 
 love.addEventListener('click', () => {
-    if (love.src.includes('Love-new.svg')) {
+    isClicked = !isClicked; // 點擊後切換狀態
+    if (isClicked) {
         love.src = './images/Love-new-hover.svg';
     } else {
         love.src = './images/Love-new.svg';
     }
 });
 
+love.addEventListener('mouseenter', () => {
+    if (!isClicked) {
+        love.src = './images/Love-new-hover.svg';
+    }
+});
+
+love.addEventListener('mouseleave', () => {
+    if (!isClicked) {
+        love.src = './images/Love-new.svg';
+    }
+});
+
+
 // 點收藏
 let collect = document.getElementById('collect');
+let isClicked2 = false; // 用來追蹤是否點擊過
 
 collect.addEventListener('click', () => {
-    if (collect.src.includes('collect-new.svg')) {
+    isClicked2 = !isClicked2; // 點擊後切換狀態
+    if (isClicked2) {
         collect.src = './images/collect-new-hover.svg';
     } else {
         collect.src = './images/collect-new.svg';
     }
 });
+
+collect.addEventListener('mouseenter', () => {
+    if (!isClicked2) {
+        collect.src = './images/collect-new-hover.svg';
+    }
+});
+
+collect.addEventListener('mouseleave', () => {
+    if (!isClicked2) {
+        collect.src = './images/collect-new.svg';
+    }
+});
+
 
 // 點分享
 let share = document.getElementById('share');
