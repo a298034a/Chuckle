85f4570c0534ec52aaeb5d2bb83cc2854ef7704d
tungchiu8896 <sugar8809062337@gmail.com>
2026-06-25 14:01:47 +0800

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -4,6 +4,6 @@ document.addEventListener('DOMContentLoaded', function () {
         <p class="contrast-text"><span class="en">Copyright © 2024 Chuckle Inc.</span> 保留一切權利。</p>
     </footer>
     `;
-    
-   document.body.insertAdjacentHTML('beforeend', footerHTML);
+
+    document.body.insertAdjacentHTML('beforeend', footerHTML);
 });
